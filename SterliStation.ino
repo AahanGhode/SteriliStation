7ed598449ef2765f707b2e0a0d93a5bfae6d4974
AahanGhode <94769329+AahanGhode@users.noreply.github.com>
2022-01-21 09:59:51 -0500

    Update SterliStation.ino

--- SterliStation.ino
+++ SterliStation.ino
@@ -1,7 +1,7 @@
-int switchBtn=2;
-int buzzer=3;
-int strtUpBtn=4;
-int statusLED=6;
+int switchBtn = 2;
+int buzzer = 3;
+int strtUpBtn = 4;
+int statusLED = 6;
 bool button_history = false;
 bool latch = false;
 bool door_state = false;
@@ -22,10 +22,9 @@ void setup() {
   lcd.begin(16, 2);
   BOOTUP();
   pinMode(buzzer, OUTPUT);
-  pinMode(button, INPUT);
   pinMode(strtUpBtn, INPUT);
-  pinMode(relay, OUTPUT);
-  pinMode(status_LED, OUTPUT);
+  pinMode(switchBtn, INPUT);
+  pinMode(statusLED, OUTPUT);
   
   HOME();
   BUTTON_TRUE();
@@ -35,3 +34,24 @@ void loop() {
   // put your main code here, to run repeatedly:
   SANITIZE();
 }
+
+void BOOTUP() {
+  String bootUpMsgT = "-SteriliStation-";
+  String bootUpMsgV = "Version 1";
+  lcd.setCursor(0,0);
+  lcd.print(bootUpMsgT);
+  
+  lcd.setCursor(4,1);
+  lcd.print(bootUpMsgV);
+  
+  delay(2000);
+
+
+ for (int positionCounter = 0; positionCounter < BootupMSG_T.length()+3; positionCounter++) {
+  lcd.scrollDisplayLeft();
+  
+  delay(200);
+ }
+ lcd.clear();
+}
+
